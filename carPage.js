4960bacaca94f28b15563a32eacae7d9a2a41ba6
Bryon <bryon.pectol@gmail.com>
2019-03-05 17:34:51 -0700

    added 1 file and edited other files

--- carPage.js
+++ carPage.js
@@ -1,1 +1,35 @@
-import { cars } from '/cars.js'
+import { cars } from '../cars.js';
+
+const car = cars.filter(company => company.make === 'Dodge');
+const carName = cars.filter(names => names.name === 'Dart');
+
+
+
+
+console.log(car);
+
+
+const mainContainer = document.createElement('div');
+
+mainContainer.className = 'container';
+
+
+car.forEach((CAR) => {
+    
+let carElement = document.createElement('div');
+
+let carMake = document.createElement('p');
+
+carElement.className = 'box';
+
+carMake.textContent = CAR.make;
+
+carElement.textContent = CAR.name;
+
+
+mainContainer.appendChild(carElement);
+});
+
+
+
+document.body.appendChild(mainContainer);
--- cars.js
+++ cars.js
@@ -1,10 +1,20 @@
 export const cars = [{
 
     "make": "Dodge",
-    "model": "Dart",
+    "name": "Dart",
     "year": "1960",
     "hp": "Unknown",
-    "drive": "",
+    "drive": "FWD",
     "transmission": "3-speed manual"
+},
+
+{
+    "make": "Dodge",
+    "name": "Avenger",
+    "year": "1994",
+    "hp": "140",
+    "drive": "FWD",
+    "transmission": "5-speed manual"
 }
+
 ]
